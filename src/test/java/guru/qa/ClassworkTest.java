596285335170ed082e34891f2f31798c3c2d5ae0
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-07-20 23:15:34 +0300

    added classwork

---
+++ src/test/java/guru/qa/ClassworkTest.java
@@ -0,0 +1,33 @@
+package guru.qa;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class ClassworkTest extends TestBase{
+
+    @DisplayName("Github test")
+    @Test
+    public void githubIssueSearchTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#133")).should(Condition.exist);
+    }
+
+}
